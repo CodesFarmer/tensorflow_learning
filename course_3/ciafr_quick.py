0b27faa13b4cc2927702f061b9a44926470cdbb3
sh_u <shenhui@untouch-tech.com>
2017-11-28 17:50:41 +0800

    resnet cifar 10

--- course_3/ciafr_quick.py
+++ course_3/ciafr_quick.py
@@ -1,5 +1,7 @@
 import tensorflow as tf
 import os
+import h5py as hdf5
+import numpy as np
 import neuralnetwork
 
 _WEIGHT_DECAY = 4e-3
@@ -23,17 +25,26 @@ class cifar_nn(neuralnetwork.NeuralNetwork):
         )
 
 def model_fn(features, labels, mode):
-    inputs = tf.reshape(features, [-1, 32, 32, 3])
+    inputs = tf.reshape(features['x'], [-1, 32, 32, 3])
+    inputs = tf.cast(inputs, tf.float32)
     logits = cifar_nn({'data' : inputs}).layers['fc2']
     cifar_nn.training = True
     cross_entropy = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=labels, logits=logits))
     loss = cross_entropy + _WEIGHT_DECAY*tf.add_n([tf.nn.l2_loss(v) for v in tf.trainable_variables()])
     tf.identity(cross_entropy, name='cross_entropy')
     tf.summary.scalar('cross_entropy', cross_entropy)
-    #set the learning rate
-    learning_rate = 0.0005
     batch_size = 100
     global_step = tf.train.get_or_create_global_step()
+    #set the learning rate
+    initial_learning_rate = 0.0005
+    # Multiply the learning rate by 0.1 at 100, 150, and 200 epochs.
+    boundaries = [int(500 * epoch) for epoch in [20, 30, 40]]
+    values = [initial_learning_rate * decay for decay in [1, 1, 1, 1]]
+    learning_rate = tf.train.piecewise_constant(
+        tf.cast(global_step, tf.int32), boundaries, values)
+    # Create a tensor named learning_rate for logging purposes
+    tf.identity(learning_rate, name='learning_rate')
+    tf.summary.scalar('learning_rate', learning_rate)
     #For comparision, we set the learning fixed
     optimizer = tf.train.MomentumOptimizer(
         learning_rate=learning_rate,
@@ -58,34 +69,59 @@ def model_fn(features, labels, mode):
     return tf.estimator.EstimatorSpec(
         mode=mode,
         train_op=train_op,
+        loss=loss,
         predictions=prediction,
-        optimizer=optimizer,
         eval_metric_ops=metrics
     )
 
 
-def main():
-    os.environ['TF_ENABLE_WINOGRAD_NONFUSED'] = '1'
-    run_config = tf.estimator.RunConfig().replace(save_checkpoints_secs=1e9)
-    cifar_classifier = tf.estimator.Estimator(
-        model_fn=model_fn, model_dir='model', config=run_config)
-    train_epochs = 40
-    for _ in range(train_epochs // 1):
-        tensors_to_log = {
-            'learning_rate': 'learning_rate',
-            'cross_entropy': 'cross_entropy',
-            'train_accuracy': 'train_accuracy'
-        }
+os.environ['TF_ENABLE_WINOGRAD_NONFUSED'] = '1'
+run_config = tf.estimator.RunConfig().replace(save_checkpoints_secs=1e9)
+cifar_classifier = tf.estimator.Estimator(
+    model_fn=model_fn, model_dir='model', config=run_config)
+train_epochs = 40
+h5file = hdf5.File('/tmp/cifar10_data/CIFAR-10.hdf5')
+train_data = h5file['/normalized_full/training/default']
+train_data = train_data.value[0, :, :, :, :]
+tr_label = h5file['/normalized_full/training/targets']
+train_label = np.zeros(shape=[tr_label.shape[1], 10])
+for tl in range(50000):
+    train_label[tl, tr_label.value[0, tl, 0]] = 1
+test_data = h5file['/normalized_full/test/default']
+test_data = test_data.value[0, :, :, :, :]
+te_label = h5file['/normalized_full/test/targets']
+test_label = np.zeros(shape=[te_label.shape[1], 10])
+for tl in range(10000):
+    test_label[tl, te_label.value[0, tl, 0]] = 1
+train_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={'x': np.array(train_data)},
+    y=np.array(train_label),
+    batch_size=100,
+    num_epochs=None,
+    shuffle=True
+)
+test_input_fn = tf.estimator.inputs.numpy_input_fn(
+    x={'x': np.array(test_data)},
+    y=np.array(test_label),
+    batch_size=100,
+    num_epochs=1,
+    shuffle=True
+)
+for _ in range(train_epochs // 1):
+    tensors_to_log = {
+        'learning_rate': 'learning_rate',
+        'cross_entropy': 'cross_entropy',
+        'train_accuracy': 'train_accuracy'
+    }
 
-        logging_hook = tf.train.LoggingTensorHook(
-            tensors=tensors_to_log, every_n_iter=100)
+    logging_hook = tf.train.LoggingTensorHook(
+        tensors=tensors_to_log, every_n_iter=10)
 
-        cifar_classifier.train(
-            input_fn=lambda: input_fn(
-                True, FLAGS.data_dir, FLAGS.batch_size, FLAGS.epochs_per_eval),
-            hooks=[logging_hook])
+    cifar_classifier.train(
+        input_fn=train_input_fn,
+        steps=5000)
 
-        # Evaluate the model and print results
-        eval_results = cifar_classifier.evaluate(
-            input_fn=lambda: input_fn(False, FLAGS.data_dir, FLAGS.batch_size))
-        print(eval_results)
+    # Evaluate the model and print results
+    eval_results = cifar_classifier.evaluate(
+        input_fn=test_input_fn)
+    print(eval_results)
--- course_3/classhand.py
+++ course_3/classhand.py
@@ -1,19 +1,30 @@
 import h5py
 import numpy as np
 
-h5file =  h5py.File('/home/slam/TestRoom/python/test24_hand.h5', 'r+')
-dataset_label = h5file['/label']
-dataset_data = h5file['/data']
-# data_read32 = np.zeros(shape=[None, 5], dtype=np.float32)
-# dataset.id.read(h5py.h5s.ALL, h5py.h5s.ALL, data_read32, mtype=h5py.h5t.NATIVE_FLOAT)
-label = dataset_label.value
-data = dataset_data.value
-index = np.random.permutation(label.shape[0])
-label = label[index, :]
-data = data[index, :]
-batch_size = 128
-num_batch = int(np.floor(data.shape[0]/batch_size))
-for i in range(1, num_batch):
-    index_start = (num_batch - 1)*batch_size
-    index_end = num_batch*batch_size - 1
-h5file.close()
+# h5file =  h5py.File('/home/slam/TestRoom/python/test24_hand.h5', 'r+')
+# dataset_label = h5file['/label']
+# dataset_data = h5file['/data']
+# # data_read32 = np.zeros(shape=[None, 5], dtype=np.float32)
+# # dataset.id.read(h5py.h5s.ALL, h5py.h5s.ALL, data_read32, mtype=h5py.h5t.NATIVE_FLOAT)
+# label = dataset_label.value
+# data = dataset_data.value
+# index = np.random.permutation(label.shape[0])
+# label = label[index, :]
+# data = data[index, :]
+# batch_size = 128
+# num_batch = int(np.floor(data.shape[0]/batch_size))
+# for i in range(1, num_batch):
+#     index_start = (num_batch - 1)*batch_size
+#     index_end = num_batch*batch_size - 1
+# h5file.close()
+
+h5file = h5py.File('/tmp/cifar10_data/CIFAR-10.hdf5')
+print(h5file.attrs['std'])
+print(h5file.attrs['mean'])
+train_data = h5file['/normalized_full/training/default']
+print(train_data.shape)
+train_label = h5file['/normalized_full/training/targets']
+print(train_label.shape)
+
+for i in range(10):
+    print(i)
